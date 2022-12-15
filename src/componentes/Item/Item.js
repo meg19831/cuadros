8ab3533cc5ee14fa87cc149fab847318581a57d1
Eugenia <meg19831@hotmail.com.ar>
2022-12-15 16:43:17 -0300

    agregando loading

--- src/componentes/Item/Item.js
+++ src/componentes/Item/Item.js
@@ -1,31 +1,21 @@
-
 import './item.css'
-
 import React from 'react';
 import { Link } from "react-router-dom";
+import Loading from '../Loading/Loading';
+
 
 
 
 
 const Item = ({prod})=> {
 
-/*   const [contador, setContador] = useState(0);
-      
-  const buttonAgregar = () => {
-    setContador(contador + 1);
-  };
-
-  const buttonRestar = () => {
-      setContador(contador - 1);
-    }; 
-
-useEffect(()=>{
-
-}, [contador]) */
-
 
     return(
-        
+        <>
+        {
+
+            Object.keys(prod).length === 0 ? 
+            <div><Loading/></div> :
         <div className='item2'>
         
         <img src ={prod.imagen}className="imagen" alt ={prod.id}/>
@@ -39,14 +29,10 @@ useEffect(()=>{
             <div className='botonDetalle'>
               <Link to={`/item/${prod.id}`} className="buttonHand"><button className='detalle'>Ver Detalle</button></Link>
           </div>
-            {/* <div className='botones'>
-                  <button disabled={contador <= 0} onClick={buttonRestar} className = "button">-</button>
-                  <button disabled={contador >=5} onClick={buttonAgregar} className = "button">+</button>
-                </div> */}
           </div>
-         {/* <p className='contador'>En el carrito <br></br>{contador}</p> */}
         </div >
-        
+        }
+        </>
         )
 
 }
---
+++ src/componentes/Loading/Loading.js
@@ -0,0 +1,14 @@
+import {loading} from "./loading.jpg"
+import "./loagin.css"
+
+const Loading = () => {
+
+  
+    return (
+      <div>
+        <img src={loading} alt={loading} className="loading" />
+      </div>
+    );
+  };
+  
+  export default Loading;
